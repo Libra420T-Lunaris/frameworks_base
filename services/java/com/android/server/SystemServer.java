9ef0eca06dbcb26fc8565c2505c5f8187de075e5
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-09-08 19:53:33 +0800

    services: Adding hbm service
    Change-Id: Ie67bdb862321a3c3b494a6b9fd78f3cfab6df9e7
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -174,6 +174,7 @@ import com.android.server.devicepolicy.DevicePolicyManagerService;
 import com.android.server.devicestate.DeviceStateManagerService;
 import com.android.server.display.AutoAODService;
 import com.android.server.display.AutoDimService;
+import com.android.server.display.brightness.HbmService;
 import com.android.server.display.DisplayManagerService;
 import com.android.server.display.FreeformService;
 import com.android.server.display.color.ColorDisplayService;
@@ -2885,6 +2886,12 @@ public final class SystemServer implements Dumpable {
             t.traceEnd();
 
             mSystemServiceManager.startService(HideAppListService.class);
+
+            boolean hbmSupported = SystemProperties.getBoolean("persist.sys.hbmservice_support", false);
+            String hbmFile = SystemProperties.get("persist.sys.hbmservice_file");
+            if (hbmSupported && hbmFile != null && !hbmFile.isEmpty()) {
+                mSystemServiceManager.startService(HbmService.class);
+            }
         }
 
         t.traceBegin("StartMediaProjectionManager");
