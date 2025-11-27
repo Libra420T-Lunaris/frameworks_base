51780673cb86ff2126c4d5997f075f39eb6bcc3b
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-27 16:39:18 +0800

    SystemUI: Preventing media album bitmaps from impacting performance
    targets the same approach as minari's: https://github.com/AxionAOSP/android_frameworks_base/commit/b59b6f3eef4a8b216da48a1313284795024206e3. but dont force 500px, this approach prevents media bitmaps from impacting performance on high dpi devices without increasing the max bitmap size on low pixel devices and touching unnecessary stuffs
    
    Ghost: reduce max size
    
    Change-Id: I3fae0e9671893566dd402a221e04f44b5f073329
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- media/java/android/media/session/MediaSession.java
+++ media/java/android/media/session/MediaSession.java
@@ -197,8 +197,12 @@ public final class MediaSession {
         }
 
         mContext = context;
-        mMaxBitmapSize = context.getResources().getDimensionPixelSize(
+
+        int bitmapSize = context.getResources().getDimensionPixelSize(
                 com.android.internal.R.dimen.config_mediaMetadataBitmapMaxSize);
+
+        mMaxBitmapSize = Math.min(bitmapSize, 300); 
+
         mCbStub = new CallbackStub(this);
         MediaSessionManager manager = (MediaSessionManager) context
                 .getSystemService(Context.MEDIA_SESSION_SERVICE);
