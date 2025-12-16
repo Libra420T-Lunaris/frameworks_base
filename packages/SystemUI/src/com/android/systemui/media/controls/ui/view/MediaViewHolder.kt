54627a9b808eca06e13ccc754e1cf0589e631459
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-17 07:29:13 +0800

    SystemUI: Axion waveform seekbar
    Change-Id: If2e44ff42d2c3051504c36421267a1a8e7eb08b9
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/view/MediaViewHolder.kt
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/view/MediaViewHolder.kt
@@ -60,7 +60,7 @@ class MediaViewHolder constructor(itemView: View) {
     val seamlessButton = itemView.requireViewById<View>(R.id.media_seamless_button)
 
     // Seekbar views
-    val seekBar = itemView.requireViewById<SeekBar>(R.id.media_progress_bar)
+    val seekBar = itemView.requireViewById<WaveformSeekBar>(R.id.media_progress_bar)
     // These views are only shown while the user is actively scrubbing
     val scrubbingElapsedTimeView: TextView =
         itemView.requireViewById(R.id.media_scrubbing_elapsed_time)
