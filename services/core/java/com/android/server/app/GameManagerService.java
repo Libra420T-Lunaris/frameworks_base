3174157772d05a56136f43793b86e3637b9db1c5
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-01-21 18:08:18 +0800

    services: per-app refresh rate | unique refresh rate support
    Change-Id: Id9eb40ec3aaab0e80720b920a6bb1e1c00f3378a
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/app/GameManagerService.java
+++ services/core/java/com/android/server/app/GameManagerService.java
@@ -106,7 +106,7 @@ import com.android.server.SystemService.TargetUser;
 import com.android.server.utils.LazyJniRegistrar;
 import com.android.server.wm.ActivityTaskManagerInternal;
 import com.android.server.wm.CompatScaleProvider;
-import com.android.server.wm.NtRefreshRateController;
+import com.android.server.wm.AxRefreshRateController;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -275,7 +275,7 @@ public final class GameManagerService extends IGameManagerService.Stub {
         }
 
         mSysProps = injector.createSystemPropertiesWrapper();
-        NtRefreshRateController.get().setGameFpsCallback(new NtRefreshRateController.GameFpsCallback() {
+        AxRefreshRateController.get().setGameFpsCallback(new AxRefreshRateController.GameFpsCallback() {
             @Override
             public void setGameFps(int uid, float fps) {
                 setGameModeFrameRateOverride(uid, fps);
@@ -2273,7 +2273,7 @@ public final class GameManagerService extends IGameManagerService.Stub {
 
     @VisibleForTesting
     void setGameModeFrameRateOverride(int uid, float frameRate) {
-        NtRefreshRateController.get().setGameModeFrameRateOverrideToNtRefreshRate(uid, frameRate);
+        AxRefreshRateController.get().setGameModeFrameRateOverrideToNtRefreshRate(uid, frameRate);
         nativeSetGameModeFrameRateOverride(uid, frameRate);
     }
 
